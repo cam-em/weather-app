cdd43bd2d4216f92ec01e24dc772c54336a669f0
cam-em <cmoreno.1117@gmail.com>
2022-03-06 20:06:08 -0600

    added boilerplate authorization to backend

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,19 @@
 from flask import Flask, request, jsonify
+from datetime import datetime, timedelta, timezone
 from flask_pymongo import PyMongo, ObjectId
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
+
 
 app = Flask(__name__)
+
 app.config['MONGO_URI']='mongodb://localhost:27017/weatherapp'
+app.config['JWT_SECRET_KEY']='please-remember-to-change-me'
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+
+
+jwt = JWTManager(app)
 mongo = PyMongo(app)
 
 CORS(app)
@@ -14,6 +24,42 @@ db = mongo.db.users
 # def index():
 #     return '<h1>Hello World</h1>'
 
+'''Authentiction Section'''
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+
+@app.route('/token', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    response = {"access_token":access_token}
+    return response
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+'''CRUD Section'''
 @app.route('/user', methods=['POST'])
 def create_user():
     print(request.json)
@@ -52,6 +98,7 @@ def get_user(id):
     })
 
 @app.route('/users/<id>', methods=['DELETE'])
+@jwt_required()
 def delete_user(id):
     db.delete_one({
         '_id': ObjectId(id)
